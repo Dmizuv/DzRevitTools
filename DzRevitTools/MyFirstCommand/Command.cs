578711f79fcdc6eca7b433873a65102b7e54d83a
Dmizuv <zuevdr@gmail.com>
2023-12-01 22:47:51 +0300

    Добавить кнопку

---
+++ DzRevitTools/MyFirstCommand/Command.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using DzRevitTools.BASE;
+using static DzRevitTools.Utils;
+
+namespace DzRevitTools.MyFirstCommand;
+
+[Transaction(TransactionMode.Manual)]
+[Regeneration(RegenerationOption.Manual)]
+class Command : IButtonCommand, IExternalCommand
+{
+    public string ButtonText => "MyFirstCommand11111";
+    public string Title => "Название окна, транзакции";
+    public string Tooltip => "(Description is coming soon)";
+    public Bitmap Logo { get; } = Properties.Resources.LogoTemplate;
+
+    private static void Execute(ExternalCommandData commandData)
+    {
+        new Model(commandData).DoJob();
+    }
+    
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    {
+        try
+        {
+            LogStartCommand(Title, commandData, ++Properties.Settings.Default.CounterRunMyFirstCommand);
+            Execute(commandData);
+            LogEndCommand(Title);
+        }
+        catch (Exception e)
+        {
+            LogException(e);
+            ShowException(e);
+        }
+        return Result.Succeeded;
+    }
+}
---
+++ DzRevitTools/MyFirstCommand/Model.cs
@@ -0,0 +1,30 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Windows;
+
+namespace DzRevitTools.MyFirstCommand;
+
+public class Model
+{
+    private readonly UIApplication _uiApp;
+    private readonly UIDocument _uiDoc;
+    private readonly Document _doc;
+
+    public Model(ExternalCommandData commandData)
+    {
+        _uiApp = commandData.Application;
+        _uiDoc = _uiApp.ActiveUIDocument;
+        _doc = _uiDoc.Document;
+    }
+
+    internal void DoJob()
+    {
+        // using var t = new Transaction(_doc);
+        // t.Start(new Command().Title);
+
+        MessageBox.Show("qwe12 fdddddddddddddddddqwe12 fdddddddddddddddddqwe12 fdddddddddddddddddqwe12 fdddddddddddddddddqwe12 fddddddddddddddddd");
+        
+        // t.SetName($"{t.GetName()} ({_resultCounter})");
+        // t.Commit();
+    }
+}
